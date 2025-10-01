89691a35a1450d57ef657258fd501eebd69f7bb6
Devsam_ <sabillyamri1@gmail.com>
2025-10-01 17:02:30 +0700

    fix : testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,7 @@
 <body>
     <div class="min-h-screen bg-slate-400 flex justify-center items-center">
         <h1 class="text-3xl font-bold bg-primary w-[15em] font-plusJakartaSans text-white">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui laudantium sint exercitationem modi distinctio
-            reprehenderit officia atque iure aperiam sit ipsa nostrum nemo numquam voluptate porro non eveniet,
-            voluptatibus illo!
+            Bebrinarin Keren
         </h1>
     </div>
 </body>
