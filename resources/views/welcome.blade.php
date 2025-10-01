d1bcd264638eadba196a5fc5433328c501f3b34b
Devsam_ <sabillyamri1@gmail.com>
2025-10-01 16:00:20 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="min-h-screen bg-slate-400 flex justify-center items-center">
+        <h1 class="text-3xl font-bold bg-primary w-[15em] font-plusJakartaSans text-white">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui laudantium sint exercitationem modi distinctio
+            reprehenderit officia atque iure aperiam sit ipsa nostrum nemo numquam voluptate porro non eveniet,
+            voluptatibus illo!
+        </h1>
+    </div>
+</body>
+
+</html>
